9431332da8c3fc52f91ddf6eace98551279d6b27
Isolde <isoldevyy@gmail.com>
2025-03-08 19:28:57 -0800

    data

--- app/App.js
+++ app/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import AppNavigator from "./AppNavigator";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const App = () => <AppNavigator />;
 
